0ce05149a5d8b15eb86591fd54f6dc7ef05bf98c
Rizkan Firmansyah <riezkanaprianda@gmail.com>
2021-01-23 05:03:10 +0700

    add md5 and authentication register user

---
+++ middleware/auth.js
@@ -0,0 +1,44 @@
+var connection = require('../koneksi');
+var response = require('../res');
+var config = require('../config/secret')
+var mysql = require('mysql');
+var md5 = require('MD5');
+var jwt = require('jsonwebtoken');
+var ip = require('ip');
+const conn = require('../koneksi');
+
+// Register Controller 
+exports.register = function(req, res) {
+    var post = {
+        username : req.body.username,
+        email : req.body.email,
+        password : md5(req.body.password),
+        role : req.body.role,
+    }
+
+    var query = "SELECT email FROM ?? WHERE ?? ";
+    var table = ["user", "email", post.email];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function(error, rows, fields) {
+        if(error){
+            console.log(error); 
+        }else{
+            if(rows.length == 0){
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function(error, rows)  {
+                    if(error){
+                        console.log(error);
+                    }else{
+                        response.ok("User berhasil ditambahkan!", res)
+                    }
+                })
+            }else{
+                response.ok("Email sudah terdaftar!");
+            }
+        }
+    })
+}
