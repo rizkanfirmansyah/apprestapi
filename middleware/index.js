53a705c9fb571aa6e6219afb77389ccc1dc7311a
Rizkan Firmansyah <riezkanaprianda@gmail.com>
2021-01-24 08:43:52 +0700

    authorization and verify

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,15 @@
+const { response } = require('express');
 const express = require('express');
 const auth = require('./auth');
+const verify = require('./verify');
 const router = express.Router();
+var verify = require('./verify');
 
 // daftarkan menu register
 router.post('/api/v1/register', auth.register);
 router.post('/api/v1/login', auth.login);
 
+// alamat yang perlu authorizaion
+router.get('api/v/rahasia', verify(1), auth.halamanrahasia);
 
 module.exports = router;
